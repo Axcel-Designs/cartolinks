726997eaac2d4fca4d26bb883901112750ebe8e5
excel <axceldesigns@gmail.com>
2025-09-13 01:54:05 +0100

    cart

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,13 +1,23 @@
 import { IconType } from "react-icons";
-import { FaBalanceScale, FaBrush, FaCamera, FaFolder, FaHome, FaHeadset } from "react-icons/fa";
+import { FaBalanceScale, FaBrush, FaCamera, FaFolder, FaHome, FaHeadset, FaImage, FaVideo, FaEdit, FaMicrophone, FaMagic, FaTrain } from "react-icons/fa";
 import { FaPhotoFilm, FaWandMagic } from "react-icons/fa6";
 import { LuGithub, LuLinkedin, LuMail, LuTwitter } from "react-icons/lu";
+import { MdTransferWithinAStation } from "react-icons/md";
+import { GiHeavyTimer } from "react-icons/gi";
 
 interface SocialLink {
   name: string;
   href: string;
   label: string;
   icon: IconType;
+}
+interface GenerateType {
+  name: string;
+  href: string;
+  label: string;
+  color: string;
+  icon: IconType;
+  new: boolean
 }
 
 interface NavLink {
@@ -23,30 +33,10 @@ export const navItems: NavLink[] = [
 ];
 
 export const socialsData: SocialLink[] = [
-  {
-    name: "Axcel-Designs",
-    href: "https://github.com/Axcel-Designs",
-    label: "GitHub",
-    icon: LuGithub,
-  },
-  {
-    name: "excel-acha",
-    href: "https://linkedin.com/in/excel-acha",
-    label: "LinkedIn",
-    icon: LuLinkedin,
-  },
-  {
-    name: "axceldesigns@gmail.com",
-    href: "mailto:axceldesigns@gmail.com",
-    label: "Email",
-    icon: LuMail,
-  },
-  {
-    name: "@axceldesigns",
-    href: "https://x.com/axceldesigns",
-    label: "X (formerly Twitter)",
-    icon: LuTwitter,
-  },
+  { name: "Axcel-Designs", href: "https://github.com/Axcel-Designs",label: "GitHub",icon: LuGithub,},
+  {name: "excel-acha",href: "https://linkedin.com/in/excel-acha",label: "LinkedIn", icon: LuLinkedin, },
+  {name: "axceldesigns@gmail.com",href: "mailto:axceldesigns@gmail.com",label: "Email",icon: LuMail,},
+  { name: "@axceldesigns", href: "https://x.com/axceldesigns",label: "X (formerly Twitter)",icon: LuTwitter,},
 ];
 
 export const navLinks: NavLink[] = [
@@ -60,12 +50,60 @@ export const navLinks: NavLink[] = [
 ];
 
 export const generateData = [
-  { name: 'Image', desc: 'Generates images with custom styles in flux and ideogram.', icon: '', new: true },
-  { name: 'Video', desc: '', icon: 'Generates Videos with Halius pics, Runway Luma and more.', new: true },
-  { name: 'RealTime', desc: 'RealTime AI rendering on a canvas. Instant feedback loops.', icon: '', new: true },
-  { name: 'Enhancer', desc: 'Upscale and enchance images and videos up to 22k.', icon: '', new: false },
-  { name: 'Edit', desc: 'Add objects, changes styles or expand photos and generators.', icon: '', new: true },
-  { name: 'VideoLipsync', desc: 'Lip sync any video to any audio.', icon: '', new: true },
-  { name: 'Motion Enhancer', desc: 'Transfer motion to images and animate characters.', icon: '', new: false },
-  { name: 'Train', desc: '', icon: 'Teach Kres ai to replicate your style, products or characters.', new: false },
+  {
+    name: 'Image',
+    icon: FaImage,
+    desc: 'Generates images with custom styles in flux and ideogram.',
+    new: true,
+    color: 'text-pink-500'
+  },
+  {
+    name: 'Video',
+    icon: FaVideo,
+    desc: 'Generates Videos with Halius pics, Runway Luma and more.',
+    new: true,
+    color: 'text-red-500'
+  },
+  {
+    name: 'RealTime',
+    icon: GiHeavyTimer,
+    desc: 'RealTime AI rendering on a canvas. Instant feedback loops.',
+    new: true,
+    color: 'text-green-500'
+  },
+  {
+    name: 'Enhancer',
+    icon: FaMagic,
+    desc: 'Upscale and enchance images and videos up to 22k.',
+    new: false,
+    color: 'text-purple-500'
+  },
+  {
+    name: 'Edit',
+    icon: FaEdit,
+    desc: 'Add objects, changes styles or expand photos and generators.',
+    new: true,
+    color: 'text-blue-500'
+  },
+  {
+    name: 'VideoLipsync',
+    icon: FaMicrophone,
+    desc: 'Lip sync any video to any audio.',
+    new: true,
+    color: 'text-yellow-500'
+  },
+  {
+    name: 'Motion Transfer',
+    icon: MdTransferWithinAStation,
+    desc: 'Transfer motion to images and animate characters.',
+    new: false,
+    color: 'text-indigo-500'
+  },
+  {
+    name: 'Train',
+    icon: FaTrain,
+    desc: 'Teach Kres ai to replicate your style, products or characters.',
+    new: false,
+    color: 'text-emerald-500'
+  },
 ]
